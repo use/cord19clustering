47223382ae1a10ec1a717974d533abf850d4d7b9
colinmiddletonewu <74208307+colinmiddletonewu@users.noreply.github.com>
2021-02-03 21:18:32 -0800

    Update cord19.py

--- cord19.py
+++ cord19.py
@@ -203,6 +203,84 @@ def peek_doc(doc):
         actualword = vocabulary['words'][word_id][0]
         print(f"'{actualword}', {doc[1][word_id]}")
 
+def vectorize(doc, vocabulary_length):
+    # Inflate doc to vocabulary length
+    doc_vector = []
+    for i in range(vocabulary_length):
+        # If doc contains word in vocab, save it's tfidf
+        if i in doc[1].keys():
+            doc_vector.append(doc[1][i])
+        # Otherwise add a 0 for that word
+        else:
+            doc_vector.append(0)
+    # return vector of vocabulary length, not just doc length
+    return doc_vector
+
+def n_sketches(n, dimension):
+    # These are our hash functions
+    sketches = []
+    for i in range(n):
+        sketch = []
+        possibilities = [-1, 1]
+        # Each sketch is a vector of only +1's and -1's
+        for j in range(dimension):
+            sketch.append(possibilities[randint(0,1)])
+        sketches.append(sketch)
+    return sketches
+
+def project_on_(u,v):
+    # This is the scaler projection
+    # Returns the amount (length) of u that is in the direction of v
+    return numpy.dot(u,v) / numpy.linalg.norm(v)
+
+def signature(doc, sketches, a):
+    # Each sketch is vocabulary length
+    vocabulary_length = len(sketches[0])
+    doc_vector = vectorize(doc, vocabulary_length)
+    signature = []
+    for sketch in sketches:
+        projection = project_on_(doc_vector, sketch)
+        hash_bin = math.ceil(projection / a)
+        signature.append(hash_bin)
+    return signature
+
+def LSH(doc_sig, r):
+    bands_hashes = []
+    for i in range(b):
+        band = doc_sig[i:i+r]
+        # Use the built-in Python hash function to hash each band
+        # We can change this as needed
+        bands_hashes.append(hash(frozenset(band)))
+    return bands_hashes
+
+def corpus_signatures_LSH(corpus, sketches, a, r):
+    # NOTE: This method changes the existing corpus
+    # Combines signature generation and LSH steps
+    for i in range(len(corpus)):
+        doc = corpus[i]
+        doc_name = doc[0]
+        # Get doc signature using vector projections
+        doc_sig = signature(doc, sketches, a)
+        # Then do LSH on signatures with b bands to get hashes
+        doc_LSH = LSH(doc_sig, r)
+        #doc_LSH = [doc_name % 2, doc_name % 3]
+        corpus[i] = (doc_name, doc_LSH)
+    return
+
+def find_candidates(corpus):
+    candidates = []
+    # Look at all pairs of docs
+    for i in range(len(corpus)):
+        doc1 = corpus[i]
+        for j in range(i+1, len(corpus)):
+            doc2 = corpus[j]
+            # Find if any have same hash value for a band
+            for k in range(len(doc1[1])):
+                if doc1[1][k] == doc2[1][k]:
+                    candidates.append((doc1[0], doc2[0]))
+    return candidates
+
+
 # vocabulary: {
 #     'index': {
 #         word: word_id
@@ -260,26 +338,52 @@ print('--- Performance ---')
 for timing in timings:
     print(f"{timing}: {timings[timing]}")
 
+###########
+
+vocab_size = len(vocabulary['index'])
+num_sketches = 6 # Used for signature generation
+a = 1 # This is a parameter for the buckets of the sketches
+b = int(num_sketches / 3) # Number of bands - Set this
+r = int(num_sketches / b) # Number of rows per band - Pass this
+# b * r = len of transformed docs (num_sketches), so 3 rows per band here
+# NOTE: Ensure that b divides transformed doc (num_sketches) size evenly
+
+sketches = n_sketches(num_sketches, vocab_size)
+      
+'''
+corpus: [
+        (
+            doc_id, 
+            {
+                word_id : tfidf
+            }
+        )
+    ]
+'''
+      
+# NOTE: This method changes the existing corpus
+corpus_signatures_LSH(corpus, sketches, a, r)
+      
+'''
+corpus: [
+        (
+            doc_id, 
+            [
+                LSH value for band 0,
+                LSH value for band 1,
+                ...
+            ]
+        )
+    ]
+'''
+
+candidates = find_candidates(corpus)
+
+'''
+candidates: [
+    (doc_id1, doc_id2)
+]
+'''
+
 exit()
-'''
-Might be better/faster to use this tfidf method
-https://www.kaggle.com/maksimeren/covid-19-literature-clustering#PCA--&-Clustering
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-def vectorize(text, maxx_features):
-    
-    vectorizer = TfidfVectorizer(max_features=maxx_features)
-    X = vectorizer.fit_transform(text)
-    return X
-'''
-
-from sklearn.decomposition import PCA
-# Looks like shape is wrong - are documents rows or columns?
-# Either way, documents should be vocabulary length, not
-
-print(f'Shape prior to reduciton: {numpy.shape(num_vector_corpus)}')
-t1 = time.time()
-corpus_reduced = PCA(n_components=0.95, random_state=42).fit_transform(numpy.array(num_vector_corpus))
-t2 = time.time()
-print(f'Shape prior to reduciton: {numpy.shape(corpus_reduced)}')
-print(f'PCA time: {t2-t1}')
